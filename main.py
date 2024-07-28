6cf811b96a40220d8af2226652de152be2d396c4
Max <maxhol@hotmail.fr>
2024-07-29 01:58:00 +0200

    working TTS, wip STT

--- main.py
+++ main.py
@@ -4,6 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pyttsx3
 
+import speech_recognition as sr
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        return "Sorry, I couldn't understand that."
+    except sr.RequestError:
+        return "Sorry, there was an error processing your request."
+
+
 def text_to_speech(text):
     engine = pyttsx3.init()
     engine.say(text)
@@ -16,7 +32,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('max')
+    print_hi('Welcome to glados')
+    while True:
+    # Speech to Text
+        user_input = speech_to_text()
+        print(f"You said: {user_input}")
     text_to_speech("I love chocolate")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
